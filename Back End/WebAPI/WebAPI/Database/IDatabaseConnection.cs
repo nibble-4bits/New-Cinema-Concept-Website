0a3903e6dd242fd19dfa7b7e05cd75c41c7bda32
Luis De Anda <andiyasha16@gmail.com>
2018-11-13 00:10:56 -0600

    Database Connection Interface

---
+++ Back End/WebAPI/WebAPI/Database/IDatabaseConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Database
+{
+    interface IDatabaseConnection
+    {
+        void Open();
+
+        void Close();
+
+        void PrepareStoredProc(string StoredProcName, List<SqlParameter> StoredProcParams);
+
+        DataTableReader ExecuteQuery();
+
+        int ExecuteNonQuery();
+    }
+}
