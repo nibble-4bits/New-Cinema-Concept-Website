52c1f2bebed013512fe1fd10a2ef698a40d51c0b
Luis De Anda <andiyasha16@gmail.com>
2018-11-15 00:53:30 -0600

    Staging front end files

---
+++ Back End/WebAPI/WebAPI/Resources/Js/MyJS/IndexApp/MyAppIndex.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $routeProvider.when('/', { //Routing for show list of employee
+        templateUrl: '../../../../Views/index/indexMain.html',
+        controller: 'IndexController'
+    });
+}]);
